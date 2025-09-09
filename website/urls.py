27823049a5b05c44e325ed04c12e874fb618a00f
deeksha <training@careerlabconsulting.com>
2025-09-09 15:51:14 +0530

    Update urls.py

--- website/urls.py
+++ website/urls.py
@@ -15,6 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+# At the top of the file
+from django.http import HttpResponse
+
 from django.urls import path, include 
 def home(request):
     return HttpResponse("Hello, CLC!")
