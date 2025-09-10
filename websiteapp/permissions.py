51322b06281fe8808d07ceb46daa2e610e24c21f
deeksha <training@careerlabconsulting.com>
2025-09-10 11:29:05 +0530

    Update permissions.py

--- websiteapp/permissions.py
+++ websiteapp/permissions.py
@@ -1,18 +1,18 @@
-from rest_framework import permissions
-from django.conf import settings
+# from rest_framework import permissions
+# from django.conf import settings
 
-class WorkshopPublicOrAdminPermission(permissions.BasePermission):
-    """
-    - Allows POST requests if X-API-KEY matches WORKSHOP_PUBLIC_KEY
-    - Allows GET requests only for admin users
-    """
+# class WorkshopPublicOrAdminPermission(permissions.BasePermission):
+#     """
+#     - Allows POST requests if X-API-KEY matches WORKSHOP_PUBLIC_KEY
+#     - Allows GET requests only for admin users
+#     """
 
-    def has_permission(self, request, view):
-        if request.method == "POST":
-            api_key = request.headers.get("X-API-KEY")
-            return api_key == settings.WORKSHOP_PUBLIC_KEY
+#     def has_permission(self, request, view):
+#         if request.method == "POST":
+#             api_key = request.headers.get("X-API-KEY")
+#             return api_key == settings.WORKSHOP_PUBLIC_KEY
 
-        elif request.method == "GET":
-            return request.user and request.user.is_staff  # only admin
+#         elif request.method == "GET":
+#             return request.user and request.user.is_staff  # only admin
 
-        return False
+#         return False
